b68edcb9ce3e48c5b355eced346a0fd974b112ec
Vikramjit <saini.vikramjit@gmail.com>
2019-01-04 00:13:56 +0530

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ButtonGroup from './component/ButtonGroup';
+
+import store from './store';
 
 class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = store.getState();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hello World { store.getState().tech }
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <ButtonGroup technologies = {["Redux", "Mobi X", "Flux"]} />
         </header>
       </div>
     );
---
+++ src/component/ButtonGroup.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import store from '../store';
+import setTechnology from '../actions';
+
+const ButtonGroup = ({ technologies }) => (
+    <div>
+        {
+            technologies.map( (tech, i) => (
+                <button
+                    key = { i }
+                    data-tech = { tech }
+                    className = "default-btn"
+                    onClick = { onButtonClick }
+                >
+                    { tech }
+                </ button>
+            ))
+        }
+    </div>
+);
+
+const onButtonClick = (e) => {
+    e.preventDefault();
+    const tech = e.target.dataset.tech;
+    store.dispatch(setTechnology(tech));
+
+}
+
+export default ButtonGroup;
