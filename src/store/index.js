cd4bd9e9ec1beac4acf137b270d23e499211e334
Vikramjit <saini.vikramjit@gmail.com>
2019-01-10 10:20:52 +0530

    Adding redux logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers';
-import { logger, error } from '../middleware';
+import { logger as customLogger, error } from '../middleware';
+import logger from 'redux-logger';
 
 const initialState = {
   tech: "React"
 };
 
-const middleware = applyMiddleware(logger, error);
+const middleware = applyMiddleware(customLogger, error, logger);
 
 const store = createStore(reducer, initialState, middleware);
 
